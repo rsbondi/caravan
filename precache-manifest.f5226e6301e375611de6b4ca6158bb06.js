5db8d60107397f6b40e3dc58a48b075b57b80f4d
Richard Bondi <r_bondi@hotmail.com>
2019-12-22 14:51:48 -0600

    Updates

---
+++ precache-manifest.f5226e6301e375611de6b4ca6158bb06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66481e66e2bea46e7b7d73df9d4953d9",
+    "url": "/caravan/static/media/logo.66481e66.png"
+  },
+  {
+    "revision": "792d9c5b5a485ad633ca",
+    "url": "/caravan/static/js/runtime~main.731ca210.js"
+  },
+  {
+    "revision": "58bc7488648512e7888a",
+    "url": "/caravan/static/js/main.6a33f8a9.chunk.js"
+  },
+  {
+    "revision": "69bf4922891e8415c780",
+    "url": "/caravan/static/js/2.5771ea78.chunk.js"
+  },
+  {
+    "revision": "58bc7488648512e7888a",
+    "url": "/caravan/static/css/main.e13c4475.chunk.css"
+  },
+  {
+    "revision": "69bf4922891e8415c780",
+    "url": "/caravan/static/css/2.57fe0083.chunk.css"
+  },
+  {
+    "revision": "3ec263ffb47d940bbc1a4d3efb060763",
+    "url": "/caravan/index.html"
+  }
+];
